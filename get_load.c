8d1529ac0c78c933e0d8067970647f7b8c3f61eb
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-09-15 16:34:22 +0000

    Pull XORG-6_8_0 to CYGWIN branch

--- get_load.c
+++ get_load.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/xload/get_load.c,v 1.1.4.5.2.1 2004/03/04 17:49:14 eich Exp $ */
+/* $XdotOrg: xc/programs/xload/get_load.c,v 1.2 2004/04/23 19:54:57 eich Exp $ */
 /* $XConsortium: get_load.c /main/37 1996/03/09 09:38:04 kaleb $ */
 /* $XFree86: xc/programs/xload/get_load.c,v 1.21tsi Exp $ */
 /*
