f77024a7f7e25bf10b945a0240eb34b532fccbe7
Alan Coopersmith <alan.coopersmith@oracle.com>
2010-08-06 09:23:58 -0700

    Sun copyrights are now owned by Oracle
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- xload.c
+++ xload.c
@@ -30,7 +30,7 @@ from the X Consortium.
 
 */
 /*
- * Copyright © 2000 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2000, Oracle and/or its affiliates. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
